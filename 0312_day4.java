baeb81dbb75e01d9dad17f87995566cb482eacc9
jss2001 <jss2001@naver.com>
2024-03-12 16:49:30 +0900

    Create 0312_day4.java
    0312_day4

---
+++ 0312_day4.java
@@ -0,0 +1,17 @@
+#프로그래머스_코딩기초트레이닝
+#배열에서 문자열 대소문자 변환하기
+
+class Solution {
+    public String[] solution(String[] strArr) {
+        String[] answer = new String[strArr.length];
+        
+        for (int i = 0; i < strArr.length; i++) {
+            if (i % 2 == 0) {
+                answer[i] = strArr[i].toLowerCase();
+            } else {
+                answer[i] = strArr[i].toUpperCase();
+            }
+        }
+        return answer;
+    }
+}
