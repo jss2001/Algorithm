514126a04cd9039cdac3580cd7d3ad919afc0b3a
jss2001 <jss2001@naver.com>
2024-03-15 14:51:27 +0900

    Update 240315_day7.java

--- 240315_day7.java
+++ 240315_day7.java
@@ -1,3 +1,7 @@
+//프로그래머스_코딩기초트레이닝
+//길이에 따른 연산
+//문제 : https://school.programmers.co.kr/learn/courses/30/lessons/181879
+
 class Solution {
     public static long solution(int[] numList) {
         // 리스트의 길이가 11 이상이면 리스트에 있는 모든 원소의 합을 반환
